0775ab592fbcafcb445faf2f17d85ffa247a929b
MayaraFreitas <mah.alvesfreitas@hotmail.com>
2019-05-27 20:22:53 -0300

    Adicionando som de abelhas

---
+++ Just That/Assets/Scripts/Bee.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bee : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            AudioSource.Play();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            AudioSource.Stop();
+        }
+    }
+}
---
+++ Just That/Assets/Scripts/GerenciadorDoGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GerenciadorDoGame : MonoBehaviour
+{
+
+    public void ChangeScene(string scene)
+    {
+        Debug.Log("SCENE: " + scene);
+        SceneManager.LoadScene("Cena Principal");
+        //Application.LoadLevel(scene);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
